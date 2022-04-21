22dc53408bb5119b49453df04efd7f5ed6272bfc
zanko khaledi <zanko.kh75@gmail.com>
2022-04-22 03:43:50 +0430

    Refactored to production

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
-
 const express = require("express")
 const path = require("path")
 const app = express()
 const port = 8000
 const host = "localhost"
+
 app.use(express.static(path.join(__dirname,"build")))
 
 app.get("/",(req,res) => {
