b84d0c2627e2e1ee2b964132439d22fc142028fa
zanko khaledi <zanko.kh75@gmail.com>
2022-04-22 03:32:16 +0430

    Node server created.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const express = require("express")
+const path = require("path")
+const app = express()
+const port = 8000
+const host = "localhost"
+app.use(express.static(path.join(__dirname,"build")))
+
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname,"build","index.html"))
+})
+
+app.listen(port,host,()=>{
+    console.log(`React server runing at http://${host}:${port}`)
+})
+
